c59085b54735b76bcb743e170f5faa49416cc976
John Hughes <john.hughes101@gmail.com>
2021-08-03 16:15:57 +0100

    ADD HELPER FUNC TO MAKE NUM SLICE

--- bench_test.go
+++ bench_test.go
@@ -73,11 +73,8 @@ var addNumsFuncs = []addNumsFunc{
 	{"addNumsRecursive", addNumsRecursive},
 }
 
-var numList = []int{
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-}
-
 func Test_addNumsFuncs(t *testing.T) {
+	numList := createSliceOfInts(1, 1000, 1)
 	for _, fn := range addNumsFuncs {
 		type args struct {
 			nums []int
@@ -89,7 +86,7 @@ func Test_addNumsFuncs(t *testing.T) {
 		}{
 			{fn.name, args{numList[:1]}, 1},
 			{fn.name, args{numList[:8]}, 36},
-			{fn.name, args{numList}, 210},
+			{fn.name, args{numList}, 499500},
 		}
 		for _, tt := range tests {
 			t.Run(tt.name, func(t *testing.T) {
@@ -102,10 +99,7 @@ func Test_addNumsFuncs(t *testing.T) {
 }
 
 func Benchmark_addNumsFuncs(b *testing.B) {
-	nums := make([]int, 0)
-	for i := 1; i < 1000; i++ {
-		nums = append(nums, i)
-	}
+	nums := createSliceOfInts(0, 1000, 1)
 
 	for _, fn := range addNumsFuncs {
 		for idx := 0; idx <= 1000; idx += 100 {
@@ -116,4 +110,12 @@ func Benchmark_addNumsFuncs(b *testing.B) {
 			})
 		}
 	}
+}
+
+func createSliceOfInts(start, max, spacing int) []int {
+	nums := make([]int, 0)
+	for i := start; i < max; i += spacing {
+		nums = append(nums, i)
+	}
+	return nums
 }
