5d981b87f24e8c924e3d143c56aabed464b747e7
John Hughes <john.hughes101@gmail.com>
2021-08-03 15:09:01 +0100

    COMPARE FOR LOOP AND RECURSIVE - create `addNumFunc` struct - create `addNumsFuncs` slice

--- bench_test.go
+++ bench_test.go
@@ -1,6 +1,7 @@
 package bench
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -59,5 +60,60 @@ func Benchmark_getLastNameFuncs(b *testing.B) {
 				fn.fn(fullnames)
 			}
 		})
+	}
+}
+
+type addNumsFunc struct {
+	name string
+	fn   func(...int) int
+}
+
+var addNumsFuncs = []addNumsFunc{
+	{"addNumsLoop", addNumsLoop},
+	{"addNumsRecursive", addNumsRecursive},
+}
+
+var numList = []int{
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+}
+
+func Test_addNumsFuncs(t *testing.T) {
+	for _, fn := range addNumsFuncs {
+		type args struct {
+			nums []int
+		}
+		tests := []struct {
+			name string
+			args args
+			want int
+		}{
+			{fn.name, args{numList[:1]}, 1},
+			{fn.name, args{numList[:8]}, 36},
+			{fn.name, args{numList}, 210},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := fn.fn(tt.args.nums...); got != tt.want {
+					t.Errorf("%s() = %v, want %v", fn.name, got, tt.want)
+				}
+			})
+		}
+	}
+}
+
+func Benchmark_addNumsFuncs(b *testing.B) {
+	nums := make([]int, 0)
+	for i := 1; i < 1000; i++ {
+		nums = append(nums, i)
+	}
+
+	for _, fn := range addNumsFuncs {
+		for idx := 0; idx <= 1000; idx += 100 {
+			b.Run(fmt.Sprintf("%s-%d", fn.name, idx), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					fn.fn(nums[:idx]...)
+				}
+			})
+		}
 	}
 }
